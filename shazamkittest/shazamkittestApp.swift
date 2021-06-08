6389ee3ac8492596fba0a92a6e68bac47cf8a3e7
Swapnanil Dhol <swapnanildhol@gmail.com>
2021-06-09 00:28:21 +0530

    Initial Commit

---
+++ shazamkittest/shazamkittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  shazamkittestApp.swift
+//  shazamkittest
+//
+//  Created by Swapnanil Dhol on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct shazamkittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
